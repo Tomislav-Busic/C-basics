b56ee117c23aebe18f68e82b49b230ebe06a35b1
Tomislav Busic <tomislav.busic89@gmail.com>
2023-08-11 17:28:25 +0200

    Add project files.

---
+++ SecondC#/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var name = "Tomislav";
+Console.WriteLine(name);
+
+//////////////////////////////////////////////////
+// DATA TYPES
+
+int a = 7 + 5;
+string b = "34524" + "83457";
+byte c = 255; // 0-255 max
+double testDouble = 14.45;
+float testFloat = 23.2323f; // f to the end
+decimal testDecimal = .1212356m; // m to the end
+
+int LADSJFLKDJASF =  1/1;
+Console.WriteLine(LADSJFLKDJASF);
+
+int modulesExample = 11 % 4;
+Console.WriteLine(modulesExample);
+
+double testDouble2 = 11 / 4;
+Console.WriteLine(testDouble2);
+
+double testDouble3 = 11.00 / 4;
+Console.WriteLine(testDouble3);
+
+//////////////////////////////////////////////////////////
