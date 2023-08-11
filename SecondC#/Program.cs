9ee77753f89f155c4c1de6eecf400e3e2819d656
Tomislav Busic <tomislav.busic89@gmail.com>
2023-08-11 18:06:53 +0200

    new updates

--- SecondC#/Program.cs
+++ SecondC#/Program.cs
@@ -13,6 +13,8 @@ double testDouble = 14.45;
 float testFloat = 23.2323f; // f to the end
 decimal testDecimal = .1212356m; // m to the end
 
+/////////////////////////////////////////////////////////////
+
 int LADSJFLKDJASF =  1/1;
 Console.WriteLine(LADSJFLKDJASF);
 
@@ -25,4 +27,26 @@ Console.WriteLine(testDouble2);
 double testDouble3 = 11.00 / 4;
 Console.WriteLine(testDouble3);
 
+int testInteger = 1;
+testInteger = testInteger + 1;
+Console.WriteLine(testInteger);
+
+int testInteger2 = 1;
+testInteger2 += 1;
+Console.WriteLine(testInteger2);
+
+// +=
+// -=
+// +=
+
+// ++
+// --
+
+int learnVariables = 0;
+learnVariables++;
+learnVariables--;
+Console.WriteLine(learnVariables);
+
+
 //////////////////////////////////////////////////////////
+
