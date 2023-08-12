60f854b9b1f72ae6f48915e4742d138f2646ec2a
Tomislav Busic <tomislav.busic89@gmail.com>
2023-08-12 18:38:25 +0200

    Tests

--- SecondC#/Program.cs
+++ SecondC#/Program.cs
@@ -23,45 +23,15 @@ decimal testDecimal = .1212356m; // m to the end
 
 /////////////////////////////////////////////////////////////
 
-Console.WriteLine("Test");
 
-int LADSJFLKDJASF =  1/1;
-Console.WriteLine(LADSJFLKDJASF);
-
-int modulesExample = 11 % 4;
-Console.WriteLine(modulesExample);
-
-double testDouble2 = 11 / 4;
-Console.WriteLine(testDouble2);
-
-double testDouble3 = 11.00 / 4;
-Console.WriteLine(testDouble3);
-
-int testInteger = 1;
-testInteger = testInteger + 1;
-Console.WriteLine(testInteger);
-
-int testInteger2 = 1;
-testInteger2 += 1;
-Console.WriteLine(testInteger2);
-
-// +=
-// -=
-// +=
-
-// ++
-// --
-
-int learnVariables = 0;
-learnVariables++;
-learnVariables--;
-Console.WriteLine(learnVariables);
 
 Console.WriteLine(newLine);
 
 //////////////////////////////////////////////////////////
 
+TypeCasting typeC = new();
 
+Console.WriteLine(typeC);
 
 Console.WriteLine(newLine);
 
---
+++ SecondC#/Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondC_
+{
+    public class Tests
+    {
+        public Tests()
+        {
+            Console.WriteLine("Tests");
+
+            int LADSJFLKDJASF = 1 / 1;
+            Console.WriteLine(LADSJFLKDJASF);
+
+            int modulesExample = 11 % 4;
+            Console.WriteLine(modulesExample);
+
+            double testDouble2 = 11 / 4;
+            Console.WriteLine(testDouble2);
+
+            double testDouble3 = 11.00 / 4;
+            Console.WriteLine(testDouble3);
+
+            int testInteger = 1;
+            testInteger = testInteger + 1;
+            Console.WriteLine(testInteger);
+
+            int testInteger2 = 1;
+            testInteger2 += 1;
+            Console.WriteLine(testInteger2);
+
+            // +=
+            // -=
+            // +=
+
+            // ++
+            // --
+
+            int learnVariables = 0;
+            learnVariables++;
+            learnVariables--;
+            Console.WriteLine(learnVariables);
+        }
+    }
+}
