8bb25aeffea6f77a2b080c6d94824af9d1603073
Tomislav Busic <tomislav.busic89@gmail.com>
2023-08-12 19:00:21 +0200

    Lists in C#

---
+++ SecondC#/ListsPart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondC_
+{
+    public class ListsPart
+    {
+        public string PartName { get; set; }
+
+        public int PartId { get; set; }
+    }
+}
