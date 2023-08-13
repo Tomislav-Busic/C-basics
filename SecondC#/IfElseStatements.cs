3a979e914e367ee2a0a4cb3467b868edbe20c26e
Tomislav Busic <tomislav.busic89@gmail.com>
2023-08-13 15:55:19 +0200

    If Else statements

---
+++ SecondC#/IfElseStatements.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondC_
+{
+    public class IfElseStatements
+    {
+        public IfElseStatements()
+        {
+
+        }
+    }
+}
