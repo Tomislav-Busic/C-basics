93d43adea45ed08bb5a485511cb79c3024d64c38
Tomislav Busic <tomislav.busic89@gmail.com>
2023-08-14 14:53:42 +0200

    Loops

--- SecondC#/LoopsAlgorithm.cs
+++ SecondC#/LoopsAlgorithm.cs
@@ -8,5 +8,24 @@ namespace SecondC_
 {
     public class LoopsAlgorithm
     {
+        public static string CountRepeatingLetters(string str)
+        {
+            string code = "";
+            int count = 1;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == str[i + 1])
+                {
+                    count++;
+                } else
+                {
+                    code += str[1] + count.ToString();
+                    count = 1;
+                }
+            }
+
+            return code;
+        }
     }
 }
